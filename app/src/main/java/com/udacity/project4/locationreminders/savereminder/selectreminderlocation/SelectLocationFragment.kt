971b76a120336a723149316e88512f2e7fddc08b
JMeckel <Jan.Meckelholt@mercedes-benz.com>
2024-03-26 11:32:49 +0100

    find own location

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -1,12 +1,21 @@
 package com.udacity.project4.locationreminders.savereminder.selectreminderlocation
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.content.res.Resources
+import android.location.Location
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -16,6 +25,7 @@ import com.google.android.gms.maps.model.GroundOverlayOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.OnSuccessListener
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.databinding.FragmentSelectLocationBinding
@@ -31,6 +41,8 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
     override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSelectLocationBinding
     private lateinit var map: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -40,6 +52,7 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
 
         binding.viewModel = _viewModel
         binding.lifecycleOwner = this
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
 
         setHasOptionsMenu(true)
         setDisplayHomeAsUpEnabled(true)
@@ -70,22 +83,26 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        // TODO: Change the map type based on the user's selection.
         R.id.normal_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_NORMAL
             true
         }
         R.id.hybrid_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_HYBRID
             true
         }
         R.id.satellite_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_SATELLITE
             true
         }
         R.id.terrain_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_TERRAIN
             true
         }
         else -> super.onOptionsItemSelected(item)
     }
 
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
         setMapStyle(map)
@@ -94,17 +111,27 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
         val mercedes = LatLng(lat, lng)
         val zoomLevel = 15f
         val overlaySize = 100f
+        enableMyLocation()
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener(requireActivity(), OnSuccessListener<Location?> { location ->
+                if (location != null) {
+                    val here = LatLng(location.latitude, location.longitude)
+                    map.addMarker(MarkerOptions().position(here).title("I am here"))
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(here, zoomLevel))
+                }
+            })
 
-        map.addMarker(MarkerOptions().position(mercedes).title("Mercedes-Benz Werk Sindelfingen"))
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(mercedes, zoomLevel))
-        val mercedesOverlay = GroundOverlayOptions()
-            .image(BitmapDescriptorFactory.fromResource(R.drawable.mb))
-            .position(mercedes, overlaySize)
-
-        map.addGroundOverlay(mercedesOverlay)
+        //map.addMarker(MarkerOptions().position(map.myLocation).title("Mercedes-Benz Werk Sindelfingen"))
+        //map.addMarker(MarkerOptions().position(mercedes).title("Mercedes-Benz Werk Sindelfingen"))
+        //map.moveCamera(CameraUpdateFactory.newLatLngZoom(mercedes, zoomLevel))
+        //map.moveCamera(CameraUpdateFactory.newLatLngZoom(mercedes, zoomLevel))
+//        val mercedesOverlay = GroundOverlayOptions()
+//            .image(BitmapDescriptorFactory.fromResource(R.drawable.mb))
+//            .position(mercedes, overlaySize)
+//
+//        map.addGroundOverlay(mercedesOverlay)
 
         //setPoiClick(map)
-        enableMyLocation()
     }
     private fun setMapStyle(map: GoogleMap) {
         try {
@@ -140,6 +167,15 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
                     .position(poi.latLng)
                     .title(poi.name)
             )?.showInfoWindow()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Companion.REQUEST_LOCATION_PERMISSION) {
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                enableMyLocation()
+            }
         }
     }
     companion object {
